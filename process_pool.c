a5cba2052e63d803e05b71e46029307fa7005e35
culnaen <chubarov.vladlen@gmail.com>
2026-01-11 19:18:43 +0300

    feature: add 5 workers

--- process_pool.c
+++ process_pool.c
@@ -3,62 +3,78 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <err.h>
-
 
 #define READ_END 0
 #define WRITE_END 1
-#define BUFFER_SIZE 100
-
+#define WORKERS 5
 
 int main() {
-    char *message = "worker";
-    int pipefd_in[2]; // in_parent
-    int pipefd_out[2]; // out_parent
-    int status;
+    int pipe_in[WORKERS][2]; 
+    int pipe_out[WORKERS][2];
     pid_t pid;
 
-    pipe(pipefd_in);
-    pipe(pipefd_out);
+    for (int i = 0; i < WORKERS; i++) {
+        pipe(pipe_in[i]);
+        pipe(pipe_out[i]);
 
-    pid = fork();
-    if (pid == 0) {
-        // child
-        char buf[256];
-        ssize_t n;
-        close(pipefd_out[WRITE_END]);
-        close(pipefd_in[READ_END]);
+        pid = fork();
+        if (pid == 0) {
+            // child
+            char buf[256];
+            ssize_t n;
 
-        dup2(pipefd_out[READ_END], STDIN_FILENO);
-        dup2(pipefd_in[WRITE_END], STDOUT_FILENO);
+            for (int j = 0; j < WORKERS; j++) {
+                if (j != i) {
+                    close(pipe_in[j][READ_END]);
+                    close(pipe_in[j][WRITE_END]);
+                    close(pipe_out[j][READ_END]);
+                    close(pipe_out[j][WRITE_END]);
+                }
+            }
 
-        close(pipefd_out[READ_END]);
-        close(pipefd_in[WRITE_END]);
+            close(pipe_in[i][WRITE_END]);
+            close(pipe_out[i][READ_END]);
 
-        while ((n = read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
-            write(STDOUT_FILENO, buf, n);
+            dup2(pipe_in[i][READ_END], STDIN_FILENO);
+            dup2(pipe_out[i][WRITE_END], STDOUT_FILENO);
+
+            close(pipe_in[i][READ_END]);
+            close(pipe_out[i][WRITE_END]);
+
+            while ((n = read(STDIN_FILENO, buf, sizeof(buf))) > 0) {
+                write(STDOUT_FILENO, buf, n);
+            }
+
+            exit(10 + i);
         }
-        
-        exit(17);
-
-    } else {
-        // parent
-        close(pipefd_out[READ_END]);
-        close(pipefd_in[WRITE_END]);
-        
-        write(pipefd_out[WRITE_END], message, strlen(message));
-        close(pipefd_out[WRITE_END]);
-        char buf[256];
-        ssize_t n;
-        while ((n = read(pipefd_in[READ_END], buf, sizeof(buf) - 1)) > 0) {
-            buf[n] = '\0';
-            printf("%s\n", buf);
-        }
-
-
-        close(pipefd_in[READ_END]);
-
-        waitpid(-1, &status, WNOHANG);
     }
 
+    // parent
+    for (int i = 0; i < WORKERS; i++) {
+        close(pipe_in[i][READ_END]);
+        close(pipe_out[i][WRITE_END]);
+    }
+
+    for (int i = 0; i < WORKERS; i++) {
+        char msg[64];
+        snprintf(msg, sizeof(msg), "worker %d says hello", i);
+        write(pipe_in[i][WRITE_END], msg, strlen(msg));
+        close(pipe_in[i][WRITE_END]);
+    }
+
+    for (int i = 0; i < WORKERS; i++) {
+        char buf[256];
+        ssize_t n;
+
+        while ((n = read(pipe_out[i][READ_END], buf, sizeof(buf) - 1)) > 0) {
+            buf[n] = '\0';
+            printf("from worker %d: %s\n", i, buf);
+        }
+        close(pipe_out[i][READ_END]);
+    }
+
+    // ждём всех
+    while (wait(NULL) > 0);
+
+    return 0;
 }
